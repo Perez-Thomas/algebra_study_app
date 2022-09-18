6d75f47253c9ac42ee1e5acc2cf7246abd010a24
Thomas Perez <thomas.v.perez@gmail.com>
2022-09-18 18:29:53 -0400

    update

--- script.js
+++ script.js
@@ -184,24 +184,24 @@ const myQuestions = [
       c: " two times the quantity six less than n",
       d: " two times the quantity n less than six",
     },
-    correctAnswer: "c",
-
-    // Adding sub-section below:
-    subTopic: "modeling-expressions",
-  },
-  {
-    question: "Which verbal expression represents 1/2(n - 3)?",
-    answers: {
-      a: " one-half the difference of n and 3",
-      b: " the difference of one-half n and 3",
-      c: " one-half n decreased by 3",
-      d: "one-half n subtracted from 3",
-    },
     correctAnswer: "a",
 
     // Adding sub-section below:
     subTopic: "modeling-expressions",
   },
+  // {
+  //   question: "Which verbal expression represents 1/2(n - 3)?",
+  //   answers: {
+  //     a: " one-half the difference of n and 3",
+  //     b: " the difference of one-half n and 3",
+  //     c: " one-half n decreased by 3",
+  //     d: "one-half n subtracted from 3",
+  //   },
+  //   correctAnswer: "a",
+
+  //   // Adding sub-section below:
+  //   subTopic: "modeling-expressions",
+  // },
   {
     question: "Which verbal expression can be represented by 2(x - 5)?",
     answers: {
@@ -210,207 +210,207 @@ const myQuestions = [
       c: " twice the difference of x and 5",
       d: "5 less than 2 times x",
     },
-    correctAnswer: "c",
+    correctAnswer: "a",
 
     // Adding sub-section below:
     subTopic: "modeling-expressions",
   },
-  {
-    question: "Which verbal expression is represented by 2(x + 4)?",
-    answers: {
-      a: " one-half the sum of x and 4",
-      b: " the difference of one-half n and 3",
-      c: " double the sum of x and 4",
-      d: "one-half n subtracted from 3",
-    },
-    correctAnswer: "c",
+  // {
+  //   question: "Which verbal expression is represented by 2(x + 4)?",
+  //   answers: {
+  //     a: " one-half the sum of x and 4",
+  //     b: " the difference of one-half n and 3",
+  //     c: " double the sum of x and 4",
+  //     d: "one-half n subtracted from 3",
+  //   },
+  //   correctAnswer: "c",
 
-    // Adding sub-section below:
-    subTopic: "modeling-expressions",
-  },
+  //   // Adding sub-section below:
+  //   subTopic: "modeling-expressions",
+  // },
 
-  ////////////////////////////
+  // ////////////////////////////
 
-  //Regents - ORDER OF OPERATIONS 1a IA/A
-  {
-    question:
-      "On February 18, from 9 a.m. until 2 p.m., the temperature rose from -14°F to 36°F. What was the total increase in temperature during this time period?",
-    answers: {
-      a: "50°",
-      b: "36°",
-      c: "32°",
-      d: "22°",
-    },
-    correctAnswer: "a",
+  // //Regents - ORDER OF OPERATIONS 1a IA/A
+  // {
+  //   question:
+  //     "On February 18, from 9 a.m. until 2 p.m., the temperature rose from -14°F to 36°F. What was the total increase in temperature during this time period?",
+  //   answers: {
+  //     a: "50°",
+  //     b: "36°",
+  //     c: "32°",
+  //     d: "22°",
+  //   },
+  //   correctAnswer: "a",
 
-    // Adding sub-section below:
-    subTopic: "order-of-operations",
-  },
+  //   // Adding sub-section below:
+  //   subTopic: "order-of-operations",
+  // },
 
-  {
-    question:
-      " What is the first step in simplifying the expression (2 - 3 * 4 + 5) &#xB2",
-    answers: {
-      a: "square 5",
-      b: "add 4 and 5",
-      c: "subtract 3 from 2",
-      d: "multiply 3 by 4",
-    },
-    correctAnswer: "d",
+  // {
+  //   question:
+  //     " What is the first step in simplifying the expression (2 - 3 * 4 + 5) &#xB2",
+  //   answers: {
+  //     a: "square 5",
+  //     b: "add 4 and 5",
+  //     c: "subtract 3 from 2",
+  //     d: "multiply 3 by 4",
+  //   },
+  //   correctAnswer: "d",
 
-    // Adding sub-section below:
-    subTopic: "order-of-operations",
-  },
-  {
-    question:
-      "#3 If the expression 3-4&#xB2 + <sup>6</sup>&frasl;<sub>2</sub>  is evaluated, what would be done last?",
-    answers: {
-      a: "subtracting",
-      b: "squaring",
-      c: "adding",
-      d: "dividing",
-    },
-    correctAnswer: "c",
+  //   // Adding sub-section below:
+  //   subTopic: "order-of-operations",
+  // },
+  // {
+  //   question:
+  //     "#3 If the expression 3-4&#xB2 + <sup>6</sup>&frasl;<sub>2</sub>  is evaluated, what would be done last?",
+  //   answers: {
+  //     a: "subtracting",
+  //     b: "squaring",
+  //     c: "adding",
+  //     d: "dividing",
+  //   },
+  //   correctAnswer: "c",
 
-    // Adding sub-section below:
-    subTopic: "order-of-operations",
-  },
-  {
-    question: "#4 The expression 15 – 3[2 + 6(–3)] simplifies to",
-    answers: {
-      a: "-45",
-      b: "-33",
-      c: "63",
-      d: "192",
-    },
-    correctAnswer: "c",
+  //   // Adding sub-section below:
+  //   subTopic: "order-of-operations",
+  // },
+  // {
+  //   question: "#4 The expression 15 – 3[2 + 6(–3)] simplifies to",
+  //   answers: {
+  //     a: "-45",
+  //     b: "-33",
+  //     c: "63",
+  //     d: "192",
+  //   },
+  //   correctAnswer: "c",
 
-    // Adding sub-section below:
-    subTopic: "order-of-operations",
-  },
-  {
-    question: "#5 The expression -|-7| is equivalent to",
-    answers: {
-      a: "1",
-      b: "0",
-      c: "7",
-      d: "-7",
-    },
-    correctAnswer: "d",
+  //   // Adding sub-section below:
+  //   subTopic: "order-of-operations",
+  // },
+  // {
+  //   question: "#5 The expression -|-7| is equivalent to",
+  //   answers: {
+  //     a: "1",
+  //     b: "0",
+  //     c: "7",
+  //     d: "-7",
+  //   },
+  //   correctAnswer: "d",
 
-    // Adding sub-section below:
-    subTopic: "order-of-operations",
-  },
-  {
-    question: "#6 The value of the expression  |-20|-|6|  is",
-    answers: {
-      a: "26",
-      b: "14",
-      c: "-14",
-      d: "-26",
-    },
-    correctAnswer: "b",
+  //   // Adding sub-section below:
+  //   subTopic: "order-of-operations",
+  // },
+  // {
+  //   question: "#6 The value of the expression  |-20|-|6|  is",
+  //   answers: {
+  //     a: "26",
+  //     b: "14",
+  //     c: "-14",
+  //     d: "-26",
+  //   },
+  //   correctAnswer: "b",
 
-    // Adding sub-section below:
-    subTopic: "order-of-operations",
-  },
-  {
-    question: "#7 An expression equivalent to 3! is",
-    answers: {
-      a: "3 * 3",
-      b: "3 * 2 * 1",
-      c: "3 * 3 * 3",
-      d: "-3",
-    },
-    correctAnswer: "b",
+  //   // Adding sub-section below:
+  //   subTopic: "order-of-operations",
+  // },
+  // {
+  //   question: "#7 An expression equivalent to 3! is",
+  //   answers: {
+  //     a: "3 * 3",
+  //     b: "3 * 2 * 1",
+  //     c: "3 * 3 * 3",
+  //     d: "-3",
+  //   },
+  //   correctAnswer: "b",
 
-    // Adding sub-section below:
-    subTopic: "order-of-operations",
-  },
-  {
-    question: "#8 The value of 5! is",
-    answers: {
-      a: "1/5",
-      b: "5",
-      c: "20",
-      d: "120",
-    },
-    correctAnswer: "d",
+  //   // Adding sub-section below:
+  //   subTopic: "order-of-operations",
+  // },
+  // {
+  //   question: "#8 The value of 5! is",
+  //   answers: {
+  //     a: "1/5",
+  //     b: "5",
+  //     c: "20",
+  //     d: "120",
+  //   },
+  //   correctAnswer: "d",
 
-    // Adding sub-section below:
-    subTopic: "order-of-operations",
-  },
-  {
-    question: "#9 The value of <sup>7!</sup>&frasl;<sub>3!</sub> is",
-    answers: {
-      a: "840",
-      b: "24",
-      c: "7",
-      d: "4",
-    },
-    correctAnswer: "a",
+  //   // Adding sub-section below:
+  //   subTopic: "order-of-operations",
+  // },
+  // {
+  //   question: "#9 The value of <sup>7!</sup>&frasl;<sub>3!</sub> is",
+  //   answers: {
+  //     a: "840",
+  //     b: "24",
+  //     c: "7",
+  //     d: "4",
+  //   },
+  //   correctAnswer: "a",
 
-    // Adding sub-section below:
-    subTopic: "order-of-operations",
-  },
-  {
-    question: "#10 The value of <sup>8!</sup>&frasl;<sub>4!</sub> is",
-    answers: {
-      a: "1,680",
-      b: "2",
-      c: "2!",
-      d: "4!",
-    },
-    correctAnswer: "a",
+  //   // Adding sub-section below:
+  //   subTopic: "order-of-operations",
+  // },
+  // {
+  //   question: "#10 The value of <sup>8!</sup>&frasl;<sub>4!</sub> is",
+  //   answers: {
+  //     a: "1,680",
+  //     b: "2",
+  //     c: "2!",
+  //     d: "4!",
+  //   },
+  //   correctAnswer: "a",
 
-    // Adding sub-section below:
-    subTopic: "order-of-operations",
-  },
-  {
-    question:
-      "#11 What is the value of | <sup>4(-6)+18</sup>&frasl;<sub>4!</sub>| ?",
-    answers: {
-      a: "1/4",
-      b: "-1/4",
-      c: "12",
-      d: "-12",
-    },
-    correctAnswer: "a",
+  //   // Adding sub-section below:
+  //   subTopic: "order-of-operations",
+  // },
+  // {
+  //   question:
+  //     "#11 What is the value of | <sup>4(-6)+18</sup>&frasl;<sub>4!</sub>| ?",
+  //   answers: {
+  //     a: "1/4",
+  //     b: "-1/4",
+  //     c: "12",
+  //     d: "-12",
+  //   },
+  //   correctAnswer: "a",
 
-    // Adding sub-section below:
-    subTopic: "order-of-operations",
-  },
-  {
-    question:
-      " The value of the expression  6! + <sup>5!(3!)</sup>&frasl;<sub>4!</sub> - 10  is",
-    answers: {
-      a: "50",
-      b: "102",
-      c: "740",
-      d: "750",
-    },
-    correctAnswer: "c",
+  //   // Adding sub-section below:
+  //   subTopic: "order-of-operations",
+  // },
+  // {
+  //   question:
+  //     " The value of the expression  6! + <sup>5!(3!)</sup>&frasl;<sub>4!</sub> - 10  is",
+  //   answers: {
+  //     a: "50",
+  //     b: "102",
+  //     c: "740",
+  //     d: "750",
+  //   },
+  //   correctAnswer: "c",
 
-    // Adding sub-section below:
-    subTopic: "order-of-operations",
-  },
+  //   // Adding sub-section below:
+  //   subTopic: "order-of-operations",
+  // },
 
   ////////////////////////////
 
   // Regents-EVALUATING EXPRESSIONS 1a MC
-  {
-    question: "What is the value of the expression |-5x + 12| when x = 5?",
-    answers: {
-      a: "-13",
-      b: "37",
-      c: "13",
-      d: "37",
-    },
-    correctAnswer: "b",
+  // {
+  //   question: "What is the value of the expression |-5x + 12| when x = 5?",
+  //   answers: {
+  //     a: "-13",
+  //     b: "37",
+  //     c: "13",
+  //     d: "37",
+  //   },
+  //   correctAnswer: "b",
 
-    // Adding sub-section below:
-    subTopic: "evaluating-expressions",
-  },
+  //   // Adding sub-section below:
+  //   subTopic: "evaluating-expressions",
+  // },
 
   // QUESTION BANK BELOW:
   // http://www.jmap.org/htmlstandard/6.EE.A.2.htm
